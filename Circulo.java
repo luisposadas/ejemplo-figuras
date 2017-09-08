39398d87d6c5c45e394b142b7d539bcb940f96e4
luisposadas <luisposadasp96@outlook.es>
2017-09-08 10:57:22 -0500

    Intercambio inicial de proyecto

---
+++ Circulo.java
@@ -0,0 +1,24 @@
+
+/**
+ * esta clase reprecenta objetos circulo con un radio
+ * @author Luis Posadas
+ * @version 1.0(8-sep-17)
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Circulo extends Figura
+{
+    private float radio;
+    public  Circulo(float unRadio)
+    {
+        radio= unRadio;
+        
+    }
+    
+    public void clculaArea()
+    {
+        area = 3.14f * radio * radio;
+        
+    }
+}
---
+++ Figura.java
@@ -0,0 +1,23 @@
+
+/**
+ * esta clase reprecenta figuras las cuales pueden ser circulos poe ejemplo
+ * @author Luis Posadas
+ * @version 1.0(8-sep-17)
+ 
+ */
+public class Figura
+{
+  protected float area;
+  public Figura()
+  {
+      area = 0;
+    }
+    public void calculaArea()
+  {
+      System.out.println("nose como");
+    }
+     public void imprimirArea()
+  {
+      System.out.println("el area es" + area);
+    }
+}
